cd4eb80f2793798e00f2f45237a330227709a932
ToastedFlaco <aaron.arredondoar@outlook.com>
2021-03-02 22:51:59 -0600

    project upload from workspace

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    AUTH0_DOMAIN: 'toastedflaco.us.auth0.com',
+    AUTH0_API_AUDIENCE: 'http://localhost:8083/api/',
+    MONGO_URI: 'mongodb+srv://aaron:Supercontra8@rsvp.s2lhq.mongodb.net/rsvp?retryWrites=true&w=majority'
+};
